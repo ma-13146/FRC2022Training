a8571a4c90e01ed85d86b75357b6bfa20808cd04
noya-isaiah <noya2110@gmail.com>
2021-07-25 22:55:12 +0300

    4Commit

--- Try/src/main/java/frc/robot/subsystems/Intake.java
+++ Try/src/main/java/frc/robot/subsystems/Intake.java
@@ -6,7 +6,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.controller.PIDController;
+// import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
