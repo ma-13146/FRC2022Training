2674139806c5f241ce955033eca01d7cf61f9083
noya-isaiah <noya2110@gmail.com>
2021-07-27 21:48:51 +0300

    7Commit

--- Try/src/main/java/frc/robot/RobotContainer.java
+++ Try/src/main/java/frc/robot/RobotContainer.java
@@ -13,25 +13,25 @@ import frc.robot.commands.IntakeIntake;
 
 public class RobotContainer {
 
-  public static Joystick joystick1 = new Joystick(0);
-  public static Joystick joystick2= new Joystick(1);
+  public static Joystick joystick1 = new Joystick(1);
+  public static Joystick joystick2= new Joystick(2);
 
 
-  private JoystickButton aButton= new JoystickButton(joystick1,0);
-  private JoystickButton bButton= new JoystickButton(joystick1,1);
-  private JoystickButton cButton= new JoystickButton(joystick1,2);
-  private JoystickButton dButton= new JoystickButton(joystick1,3);
+  private JoystickButton aButton= new JoystickButton(joystick1,1);
+  private JoystickButton bButton= new JoystickButton(joystick1,2);
+  private JoystickButton cButton= new JoystickButton(joystick1,3);
+  private JoystickButton dButton= new JoystickButton(joystick1,4);
 
-  private JoystickButton eButton= new JoystickButton(joystick1,4);
-  private JoystickButton fButton= new JoystickButton(joystick1,5);
+  private JoystickButton eButton= new JoystickButton(joystick1,5);
+  private JoystickButton fButton= new JoystickButton(joystick1,6);
 
-  private JoystickButton gButton= new JoystickButton(joystick1,6);
+  private JoystickButton gButton= new JoystickButton(joystick1,7);
 
-  private JoystickButton hButton= new JoystickButton(joystick1,7);
-  private JoystickButton iButton= new JoystickButton(joystick1,8);
+  private JoystickButton hButton= new JoystickButton(joystick1,8);
+  private JoystickButton iButton= new JoystickButton(joystick1,9);
 
-  public static XboxController firstXboxJoystick= new XboxController(9);
-  public static XboxController secondXboxJoystick= new XboxController(10);
+  public static XboxController firstXboxJoystick= new XboxController(10);
+  public static XboxController secondXboxJoystick= new XboxController(11);
 
 
   public RobotContainer() {
--- Try/src/main/java/frc/robot/subsystems/Mamuta.java
+++ Try/src/main/java/frc/robot/subsystems/Mamuta.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Mamuta extends SubsystemBase{
 
-    private static Mamuta instace= new Mamuta();
+    private static Mamuta instace= null;
 
     WPI_TalonSRX talon;
 
